34e50100da7e3a141b1b4dc2ab7c6394d91816b3
Walison Fillipe do Nascimento Muniz <w.fillipe@icloud.com>
2022-05-10 16:26:32 -0300

    Aula operadores

---
+++ modulo1/Operadores/index.js
@@ -0,0 +1,59 @@
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) //false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) //false
+
+resultado = !resultado && (bool1 || bool2) //true
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)*/ //boolean 
+
+
+/*let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) o mesmo esta imrimindo uma string em vez de numero */
+
+/*let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(soma)*/
+
+/*let idadeDoUsuario = prompt ("Qual sua idade?")
+let idadeMelhorAmigo = prompt ("Qual a idade do seu melhor amigo?")
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", Number(idadeDoUsuario) > Number(idadeMelhorAmigo) ) 
+
+console.log("A diferença entre vocês é de ", idadeDoUsuario - idadeMelhorAmigo)*/
+
+/*let insiraNum1 = prompt ("Insira um numero par aqui")
+
+const restoDaDivisao = 77 % 2
+
+console.log(restoDaDivisao) todos os resultados pares retonaram 0 ja os resultados impares entregam 1 */
+
+/*const idade = prompt ("Insira sua idade aqui em anos")
+let idadeUsusario = Number(idade)
+let dias = idadeUsusario * 365
+let horas = dias * 24
+
+console.log(idadeUsusario*12)
+console.log(dias)
+console.log(horas)*/
+
+/*let primeiroNumero = prompt ("Digite aqui o primeiro numero")
+let segundoNumero = prompt ("Digite aqui o segundo numero")
+
+console.log("O primeiro numero e maior que o segundo ?", primeiroNumero > segundoNumero)
+console.log("O primeiro numero é igual ao segundo?", primeiroNumero === segundoNumero)
+console.log("O primeiro numero é divisível pelo segundo?", primeiroNumero % segundoNumero == 0)
+console.log("O segundo numero é divisível pelo primeiro?", segundoNumero % primeiroNumero == 0)*/
