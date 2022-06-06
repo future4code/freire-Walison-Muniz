e443d1c8277265d1e310c60e568fab62350c201d
Walison Fillipe do Nascimento Muniz <w.fillipe@icloud.com>
2022-06-06 18:43:16 -0300

    Aula React

--- modulo2/jsx-labetube/meuapp/src/App.js
+++ modulo2/jsx-labetube/meuapp/src/App.js
@@ -21,7 +21,6 @@ function App() {
                     <li class="botoes-meunu-vertical">Em alta</li>
                     <li class="botoes-meunu-vertical">Inscrições</li>
                     <hr/>
-                    </hr>
                     <li class="botoes-meunu-vertical">Originais</li>
                     <li class="botoes-meunu-vertical">Histórico</li>
                 </ul>
